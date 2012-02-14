a399c8cc1d6e42a65e6c1ce95170f28aff2bc426
Anthony Porthouse <Admin@Port3M5.com>
2012-02-14 10:00:53 +0000

    Changed gitignore

---
+++ spec/Tile_spec.rb
@@ -0,0 +1,14 @@
+require 'Tile'
+
+describe "Tile" do
+  describe "initialize" do
+    it "Should set the values" do
+      edges = {:N => 0, :E => 0, :S => 0, :W => 0 }
+      base = {:base => 0, :object => 0, :edges => edges}
+      t = Tile.new base
+      t.base.should == 0
+      t.object.should == 0
+      t.edges.should have(4).items
+    end
+  end
+end
